6a9a462d33d0a9065a6af538fae1c2a3d55d3e81
Espadv69 <aitorjob69@gmail.com>
2025-03-16 18:25:30 +0100

    Add Explanation component to App for enhanced user guidance

--- ChronoMaster/src/App.jsx
+++ ChronoMaster/src/App.jsx
@@ -1,9 +1,11 @@
 import Chrono from './components/Chrono/Chrono'
+import Explanation from './components/Explanation/Explanation'
 
 const App = () => {
   return (
     <div className="app">
       <Chrono />
+      <Explanation />
     </div>
   )
 }
