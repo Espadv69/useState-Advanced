9bb17619d16a6de2f1ffdf98122625fa38ce4da3
Espadv69 <aitorjob69@gmail.com>
2025-03-16 18:22:18 +0100

    Enhance LiExplanation component to accept props for step, description, and image display

--- ChronoMaster/src/components/Explanation/LiExplanation.jsx
+++ ChronoMaster/src/components/Explanation/LiExplanation.jsx
@@ -1,3 +1,12 @@
-const LiExplanation = () => {}
+const LiExplanation = ({ step, description, imgUrl, imgAlt }) => {
+  return (
+    <li className="li__explanation">
+      <p>
+        <strong>{step}.</strong> {description}
+      </p>
+      <img src={imgUrl} alt={imgAlt} />
+    </li>
+  )
+}
 
 export default LiExplanation
