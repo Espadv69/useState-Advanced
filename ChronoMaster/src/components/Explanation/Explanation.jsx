acca5f8471f4795c5241b48deb65ad682b5a7814
Espadv69 <aitorjob69@gmail.com>
2025-03-16 18:27:39 +0100

    Add initial LiExplanation item for building steps in Explanation component

--- ChronoMaster/src/components/Explanation/Explanation.jsx
+++ ChronoMaster/src/components/Explanation/Explanation.jsx
@@ -5,7 +5,14 @@ const Explanation = () => {
   return (
     <section className="explanation">
       <h2>How to build</h2>
-      <ul className="ul__explanation"></ul>
+      <ul className="ul__explanation">
+        <LiExplanation
+          step={1}
+          description="Make initial states for the timer."
+          imgUrl="/images/initHooks.png"
+          imgAlt="Code snippet for initial states."
+        />
+      </ul>
     </section>
   )
 }
