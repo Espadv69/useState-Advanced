cfd2f20870b9ed89a9cd3cf589f6af6ce4939bf9
Espadv69 <aitorjob69@gmail.com>
2025-03-18 19:05:57 +0100

    Implement basic structure for Explanation component

--- DarkModeStorage/src/components/Explanation/Explanation.jsx
+++ DarkModeStorage/src/components/Explanation/Explanation.jsx
@@ -1,6 +1,13 @@
 import LiExplanation from './LiExplanation'
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <section className="explanation">
+      <h2>How to build</h2>
+      <ul className="ul__explanation"></ul>
+    </section>
+  )
+}
 
 export default Explanation
