4444b18fd7ceb9bef2a711471a5d13652bb1f4bd
Espadv69 <aitorjob69@gmail.com>
2025-03-18 19:07:18 +0100

    Add initial LiExplanation item for darkMode state initialization

--- DarkModeStorage/src/components/Explanation/Explanation.jsx
+++ DarkModeStorage/src/components/Explanation/Explanation.jsx
@@ -5,7 +5,14 @@ const Explanation = () => {
   return (
     <section className="explanation">
       <h2>How to build</h2>
-      <ul className="ul__explanation"></ul>
+      <ul className="ul__explanation">
+        <LiExplanation
+          step={1}
+          description="Init darkMode state with the value stored in localStorage"
+          imgUrl="/images/initHook.png"
+          imgAlt="Code snippet to initialize darkMode state"
+        />
+      </ul>
     </section>
   )
 }
