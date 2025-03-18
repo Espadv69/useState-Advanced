35a6a805b3fa29e62163d666ad8402d24a245bee
Espadv69 <aitorjob69@gmail.com>
2025-03-18 18:48:45 +0100

    Refactor dark mode state initialization to retrieve value from local storage

--- DarkModeStorage/src/components/DarkMode/DarkMode.jsx
+++ DarkModeStorage/src/components/DarkMode/DarkMode.jsx
@@ -3,19 +3,9 @@ import './DarkMode.css'
 
 const DarkMode = () => {
   // State to manage dark mode
-  const [darkMode, setDarkMode] = useState(false)
-  // State to manage dark mode in local storage
-  const [darkModeStorage, setDarkModeStorage] = useState(false)
-
-  // Function to toggle dark mode
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode)
-  }
-
-  // Function to set dark mode in local storage
-  const setDarkModeInStorage = () => {
-    setDarkModeStorage(!darkModeStorage)
-  }
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem('darkMode') === 'true'
+  })
 }
 
 export default DarkMode
