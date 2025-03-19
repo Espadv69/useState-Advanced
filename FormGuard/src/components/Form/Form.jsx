1f6d019bb900d169fb073b21c596df55ae6fb829
Espadv69 <aitorjob69@gmail.com>
2025-03-19 13:14:38 +0100

    Add state management for name, email, and error in Form component

--- FormGuard/src/components/Form/Form.jsx
+++ FormGuard/src/components/Form/Form.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react'
 import './Form.css'
 
-const Form = () => {}
+const Form = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState('')
+}
 
 export default Form
