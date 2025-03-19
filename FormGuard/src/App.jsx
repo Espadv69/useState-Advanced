c8cf4a3d42900755a998d2fea8f2a42f73f2e80b
Espadv69 <aitorjob69@gmail.com>
2025-03-19 13:42:12 +0100

    Implement Form component in App and update render structure

--- FormGuard/src/App.jsx
+++ FormGuard/src/App.jsx
@@ -1,3 +1,11 @@
-const App = () => {}
+import Form from './components/Form/Form'
+
+const App = () => {
+  return (
+    <div className="app">
+      <Form />
+    </div>
+  )
+}
 
 export default App
