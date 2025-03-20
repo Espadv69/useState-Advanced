4e3e24c70d96c590d5320af2fd553a6a18ec8fa8
Espadv69 <aitorjob69@gmail.com>
2025-03-20 12:08:38 +0100

    Enhance LiExplanation component to accept props for step, description, image URL, and alt text

--- FormGuard/src/components/Explanation/LiExplanation.jsx
+++ FormGuard/src/components/Explanation/LiExplanation.jsx
@@ -1,3 +1,12 @@
-const LiExplanation = () => {}
+const LiExplanation = ({ step, desc, imgUrl, imgAlt }) => {
+  return (
+    <li className="li__explanation">
+      <p>
+        <strong>{step}.</strong> {desc}
+      </p>
+      <img src={imgUrl} alt={imgAlt} />
+    </li>
+  )
+}
 
 export default LiExplanation
