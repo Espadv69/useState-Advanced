1354ce462cefa28376c68cc87decd3eb2d531227
Espadv69 <aitorjob69@gmail.com>
2025-03-20 12:10:17 +0100

    Implement Explanation component to render LiExplanation with step details and image

--- FormGuard/src/components/Explanation/Explanation.jsx
+++ FormGuard/src/components/Explanation/Explanation.jsx
@@ -1,6 +1,21 @@
 import LiExplanation from './LiExplanation'
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <section className="explanation">
+      <h2>How it works</h2>
+
+      <ul className="ul__explanation">
+        <LiExplanation
+          step={1}
+          desc="Initialize states for the form fields and error messages."
+          imgUrl="/images/initHooks.png"
+          imgAlt="Code snippet for initializing states."
+        />
+      </ul>
+    </section>
+  )
+}
 
 export default Explanation
