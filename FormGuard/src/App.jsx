88836ba56b06bbf0508ede2dc2107877ccc91cb0
Espadv69 <aitorjob69@gmail.com>
2025-03-20 12:20:45 +0100

    Add Explanation component to App for enhanced user guidance

--- FormGuard/src/App.jsx
+++ FormGuard/src/App.jsx
@@ -1,9 +1,11 @@
 import Form from './components/Form/Form'
+import Explanation from './components/Explanation/Explanation'
 
 const App = () => {
   return (
     <div className="app">
       <Form />
+      <Explanation />
     </div>
   )
 }
