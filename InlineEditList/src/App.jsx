bf34617babbf43ac3901d5863d2afc6830a7619b
Espadv69 <aitorjob69@gmail.com>
2025-03-21 12:13:12 +0100

    Add Explanation component to App for enhanced user guidance

--- InlineEditList/src/App.jsx
+++ InlineEditList/src/App.jsx
@@ -1,9 +1,11 @@
 import EditList from './components/EditList/EditList'
+import Explanation from './components/Explanation/Explanation'
 
 const App = () => {
   return (
     <div className="app">
       <EditList />
+      <Explanation />
     </div>
   )
 }
