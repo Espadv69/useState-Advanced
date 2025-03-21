e3165069e139778a14ca566ec188f55810aa9aa6
Espadv69 <aitorjob69@gmail.com>
2025-03-21 12:46:20 +0100

    Add state management for search input in Explorer component

--- MockAPIExplorer/src/components/Explorer/Explorer.jsx
+++ MockAPIExplorer/src/components/Explorer/Explorer.jsx
@@ -3,6 +3,9 @@ import { MOCK } from '../../data/mock'
 
 import './Explorer.css'
 
-const Explorer = () => {}
+const Explorer = () => {
+  // State to handle the search input
+  const [search, setSearch] = useState('')
+}
 
 export default Explorer
