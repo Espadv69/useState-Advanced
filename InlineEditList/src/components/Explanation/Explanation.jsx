3c6743f97931745acc6e4b813d20022649af2c0d
Espadv69 <aitorjob69@gmail.com>
2025-03-21 12:12:49 +0100

    Add LiExplanation component instance to Explanation with step description and image

--- InlineEditList/src/components/Explanation/Explanation.jsx
+++ InlineEditList/src/components/Explanation/Explanation.jsx
@@ -5,7 +5,12 @@ const Explanation = () => {
   return (
     <section className="explanation">
       <ul className="ul__explanation">
-        
+        <LiExplanation
+          step={1}
+          desc="Init states for the list items and the input field."
+          imgUrl="/images/initHooks.png"
+          imgAlt="Code snippet for initializing states for the list items and the input field."
+        />
       </ul>
     </section>
   )
