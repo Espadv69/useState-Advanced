5dafd9f31a7a7adec20ba2d0e82db6264b96035c
Espadv69 <aitorjob69@gmail.com>
2025-03-22 11:40:19 +0100

    Add LiExplanation component instance to Explanation component

--- MockAPIExplorer/src/components/Explanation/Explanation.jsx
+++ MockAPIExplorer/src/components/Explanation/Explanation.jsx
@@ -4,7 +4,14 @@ import './Explanation.css'
 const Explanation = () => {
   return (
     <section className="explanation">
-      <ul className="ul__explanation"></ul>
+      <ul className="ul__explanation">
+        <LiExplanation
+          step={1}
+          desc="Import the MOCK data from the API"
+          imgUrl="/images/mockImport.png"
+          imgAlt="Code snippet showing the import of the MOCK data"
+        />
+      </ul>
     </section>
   )
 }
