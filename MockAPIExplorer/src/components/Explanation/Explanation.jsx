bc3b0f09b3b4696f26b8682c533283514c095ffc
Espadv69 <aitorjob69@gmail.com>
2025-03-22 11:43:27 +0100

    Add section header and additional LiExplanation instance for step 2 in Explanation component

--- MockAPIExplorer/src/components/Explanation/Explanation.jsx
+++ MockAPIExplorer/src/components/Explanation/Explanation.jsx
@@ -4,12 +4,20 @@ import './Explanation.css'
 const Explanation = () => {
   return (
     <section className="explanation">
+      <h2>How to Build</h2>
       <ul className="ul__explanation">
         <LiExplanation
           step={1}
           desc="Import the MOCK data from the API"
           imgUrl="/images/mockImport.png"
           imgAlt="Code snippet showing the import of the MOCK data"
+        />
+
+        <LiExplanation
+          step={2}
+          desc="Init Hook to handle search input"
+          imgUrl="/images/initHook.png"
+          imgAlt="Code snippet showing the initialization of the Hook"
         />
       </ul>
     </section>
